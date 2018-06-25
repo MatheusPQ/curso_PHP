632506f752a6f34aa50ff1f153d7352d6bb94e52
Matheus <matheuspq79@outlook.com.br>
2018-06-24 22:46:50 -0300

    Aula de emails

---
+++ email/index.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once("vendor/autoload.php");
+/**
+ * This example shows settings to use when sending via Google's Gmail servers.
+ * This uses traditional id & password authentication - look at the gmail_xoauth.phps
+ * example to see how to use XOAUTH2.
+ * The IMAP section shows how to save this message to the 'Sent Mail' folder using IMAP commands.
+ */
+//Import PHPMailer classes into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 2;
+
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "<LOGIN DO EMAIL QUE ESTÁ ENVIANDO AQUI>";
+//Password to use for SMTP authentication
+$mail->Password = "<SENHA DO EMAIL QUE ESTÁ ENVIANDO AQUI>";
+//Set who the message is to be sent from
+$mail->setFrom('<LOGIN DO EMAIL QUE ESTÁ ENVIANDO AQUI>', 'Curso PHP 7');
+
+//Set an alternative reply-to address (ñ é obrigatório)
+// $mail->addReplyTo('replyto@example.com', 'First Last');
+
+//Set who the message is to be sent to
+$mail->addAddress('<EMAIL DE QUEM RECEBERÁ O EMAIL>', 'Teste Curso');
+
+//Set the subject line
+$mail->Subject = 'Testando a classe PHP Mailer com Gmail';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+
+//Replace the plain text body with one created manually
+$mail->AltBody = 'Caso o Arquivo HTML acima não funcione, isso será mostrado!';
+
+//Attach an image file (ANEXOS!)
+$mail->addAttachment('images/phpmailer_mini.png');
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+    //Section 2: IMAP
+    //Uncomment these to save your message in the 'Sent Mail' folder.
+    #if (save_mail($mail)) {
+    #    echo "Message saved!";
+    #}
+}
+?>
